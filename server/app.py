33eec323066914dae621d22d1ea91d30d5e67cb8
gklelei <leleigideon97@gmail.com>
2026-03-23 15:28:04 +0300

    final sollution

--- server/app.py
+++ server/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to Flatiron Cars"
+
+@app.route("/<model>")
+def models(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1')
